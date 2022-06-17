6be06d750774fde411110849f4abd2be63a3c4e2
owentr1369 <tamvaa1306@gmail.com>
2022-06-17 10:52:36 +0700

    render first load items

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const loadMore = document.getElementById("load-more");
+const items = document.getElementById("items");
+let originalData;
+let firstLoadData;
 
 document.addEventListener("DOMContentLoaded", () => {
-  firstLoad();
+  loadData();
 });
-let originalData;
+
 loadMore.onclick = () => {
   console.log("1");
 };
@@ -11,10 +14,24 @@ async function loadData() {
   await fetch("https://fakestoreapi.com/products/")
     .then((response) => response.json())
     .then((data) => (originalData = data));
-}
 
-async function firstLoad() {
-  await loadData();
-  let firstData = originalData.slice(0, 3);
-  console.log(firstData);
+  let renderData = originalData.slice(0, 3);
+  console.log(renderData);
+  function render() {
+    const htmls = renderData.map((renderDataItem) => {
+      return `<div class="item">
+          <h2>${renderDataItem.title}</h2>
+          <span>Price: <strong>$${renderDataItem.price}</strong></span>
+          <img
+            src="${renderDataItem.image}"
+            alt=""
+          />
+          <p>
+           ${renderDataItem.description}
+          </p>
+        </div>`;
+    });
+    items.innerHTML = htmls.join("");
+  }
+  render();
 }
