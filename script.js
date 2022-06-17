18434248de1366aabd742f806cc17479a92742c3
owentr1369 <tamvaa1306@gmail.com>
2022-06-17 10:07:02 +0700

    DOMContentLoaded

---
+++ script.js
@@ -0,0 +1,9 @@
+const loadMore = document.getElementById("load-more");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded");
+});
+
+loadMore.onclick = () => {
+  console.log("1");
+};
